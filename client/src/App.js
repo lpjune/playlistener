fd83df901084d6db27f0062585c90f4dc59e9d40
lpjune <laurenporsini@gmail.com>
2020-05-10 02:24:14 -0500

    tries to get all at same time, cant resolve

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,68 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Youtube from './util/Youtube';
-import Spotify from './util/Spotify';
-import Customers from './components/customers';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Youtube from "./util/Youtube";
+import Spotify from "./util/Spotify";
+import Customers from "./components/customers";
 
-Spotify.getAccessToken();
-Youtube.getVideoInfo('https://www.youtube.com/watch?v=pok8H_KF1FA').then(res => {
-  Spotify.search(res.name, res.artist)
-})
+const tracks = [];
+Youtube.getPlaylist(
+    "https://www.youtube.com/playlist?list=PLsAk6h4n-dS3IG4H69AMcWI1-3CmasVWb"
+)
+    .then((res) => {
+        let urls = Youtube.getVideoUrls(res);
+        console.log(urls)
+        for (let i = 0; i < urls.length-1; i++) {
+          
+          Youtube.getVideoInfo(urls[i])
+          .then((info) => {
+            tracks.push(info);
+        })}})
+        
 
-// fetch('/api/info')
-//       .then(res => res.json())
-//       .then(res => console.log("Info fetched...", res));
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">React Express Starter</h1>
-        </header>
-        <Customers />
-      </div>
-    );
-  }
+// Spotify.getAccessToken();
+// Youtube.getPlaylist(
+//     "https://www.youtube.com/playlist?list=PLsAk6h4n-dS3IG4H69AMcWI1-3CmasVWb"
+// ).then((res) => {
+//     let urls = Youtube.getVideoUrls(res);
+
+//     Youtube.getVideoInfo(urls[0]).then((info) => {
+//         tracks.push(info);
+//     }).then(() => console.log(tracks))
+// });
+
+// Youtube.getVideoInfo('https://www.youtube.com/watch?v=pok8H_KF1FA').then(res => {
+//   Spotify.search(res.name, res.artist)
+// })
+
+export class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchResults: [],
+            playlistName: "New Playlist",
+            playlistTracks: [],
+        };
+    }
+    search = (term) => {
+        return Spotify.search(term).then((res) =>
+            this.setState({
+                searchResults: res,
+            })
+        );
+    };
+    render() {
+        return (
+            <div>
+                <h1>Playlistener</h1>
+                <div className="App">
+                    {/* <!-- Add a SearchBar component --> */}
+                    {/* <SearchBar onSearch={this.search} /> */}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
--- client/src/util/Youtube.js
+++ client/src/util/Youtube.js
@@ -3,19 +3,35 @@ const ytdl = require("ytdl-core");
 const YOUTUBE_API = "AIzaSyClBqE-9PVuFN93FhHNcpIzV9KKbxRWk6c";
 
 const Youtube = {
-    getPlaylist(id) {
-        let url = "https://www.googleapis.com/youtube/v3/playlistItems";
-        axios
-            .get(url, {
+    getPlaylist(url) {
+        var reg = new RegExp("[&?]list=([a-z0-9_-]+)", "i");
+        var id = reg.exec(url)[1];
+        console.log(id);
+        let api_url = "https://www.googleapis.com/youtube/v3/playlistItems";
+        return axios
+            .get(api_url, {
                 params: {
                     part: "id,snippet",
                     playlistId: id,
                     key: YOUTUBE_API,
-                    maxResults: 3,
+                    maxResults: 20,
                 },
             })
-            .then((res) => console.log(res.data.items))
+            .then((res) => {
+                return res.data.items;
+            })
             .catch((err) => console.log(err));
+    },
+
+    getVideoUrls(items) {
+        const urls = [];
+        for (let i = 0; i < items.length - 1; i++) {
+            urls.push(
+                "https://www.youtube.com/watch?v=" +
+                    items[i].snippet.resourceId.videoId
+            );
+        }
+        return urls;
     },
 
     getVideoInfo(ytUrl) {
@@ -28,6 +44,24 @@ const Youtube = {
                 return res.data;
             })
             .catch((err) => console.log(err));
+    },
+
+    allYT() {
+        const tracks = [];
+        return Youtube.getPlaylist(
+            "https://www.youtube.com/playlist?list=PLsAk6h4n-dS3IG4H69AMcWI1-3CmasVWb"
+        ).then((res) => {
+            let urls = Youtube.getVideoUrls(res);
+            console.log(urls);
+            for (let i = 0; i < urls.length - 1; i++) {
+                Youtube.getVideoInfo(urls[i]).then((info) => {
+                    tracks.push(info);
+                });
+            }
+        }).then(() => {
+            return tracks
+        });
+        
     },
 };
 
