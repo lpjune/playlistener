227895cd20c087918466762929fec8e9946983cc
lpjune <laurenporsini@gmail.com>
2020-05-18 17:35:07 -0500

    simplified util

--- client/src/util/Util.js
+++ client/src/util/Util.js
@@ -3,42 +3,15 @@ import Spotify from "./Spotify";
 
 const Util = {
     getTracks(playlistUrl) {
-        const tracks = [];
-        const tracksNotFound = [];
-        let spotifyIds = [];
 
-        return Youtube.youtubeGetPlaylist(playlistUrl).then((res) => {
-            const playlistTitle = res.playlistTitle;
-            const videoUrls = res.videoUrls;
+        return Youtube.youtubeGetPlaylist(playlistUrl)
+            .then((res) => {
+                return Youtube.youtubeGetVideos(res.videoUrls);
+            })
+            .then((res) => {
+                return Spotify.spotifySearch(res)
+            })
 
-            return Youtube.youtubeGetVideos(videoUrls)
-                .then((res) => {
-                    res.forEach((track) => {
-                        if (track.name && track.artist) tracks.push(track);
-                        else tracksNotFound.push(track);
-                    });
-                })
-
-                .then(() => {
-                    console.log(playlistTitle);
-                    console.log(tracks);
-                    console.log(tracksNotFound);
-
-                    return Spotify.spotifySearch(tracks)
-                        .then((res) => {
-                            spotifyIds = res;
-                            console.log(spotifyIds);
-                            return spotifyIds;
-                        })
-                        .catch((err) => {
-                            console.log(err);
-                        });
-                })
-
-                .catch((err) => {
-                    console.log(err);
-                });
-        });
-    },
+    }
 };
 export default Util;
