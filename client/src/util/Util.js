6fde65b9aa29dfafe722ab9731f99eb5fc979c2d
lpjune <laurenporsini@gmail.com>
2020-12-25 00:59:13 -0600

    dev branch

--- client/src/util/Util.js
+++ client/src/util/Util.js
@@ -4,7 +4,9 @@ let globalAccessToken = "";
 
 export function redirectUrlToSpotifyForLogin() {
     const CLIENT_ID = process.env.REACT_APP_SPOTIFY_KEY;
-    const REDIRECT_URI = "https://playlistener0.herokuapp.com/";
+    // prod
+    // const REDIRECT_URI = "https://playlistener0.herokuapp.com/";
+    const REDIRECT_URI = "http://localhost:3000/";
     const scopes = [
         "user-modify-playback-state",
         "user-library-read",
